6018e7599886d8aaadbd9ddde74aaf2f7a125c38
한동근 <ehdrms001030@naver.com>
2025-09-10 15:50:17 +0900

    feat: content를 String에서 JSONB 형태로 변경

--- ai/fastapi/models.py
+++ ai/fastapi/models.py
@@ -1,6 +1,5 @@
-# models.py
 from sqlalchemy import Column, Integer, String, DateTime
-# from sqlalchemy.dialects.postgresql import VECTOR
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 from pgvector.sqlalchemy import VECTOR
 
@@ -11,7 +10,7 @@ class News(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
-    content = Column(String)      # 본문
+    content = Column(JSONB)      # 본문
     category = Column(String)     # 섹션 (정치, 경제 등)
     author = Column(String)       # 기자
     newsPaper = Column(String)    # 언론사
