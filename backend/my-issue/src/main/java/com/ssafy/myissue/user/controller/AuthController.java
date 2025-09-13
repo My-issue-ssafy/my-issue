9864b31fe86604adaa895dc0e8582a38da0e2ab0
홍시은 <tjwns53@naver.com>
2025-09-14 00:32:07 +0900

    chore: userId -> id column명 수정
    S13P21D101-66

--- backend/my-issue/src/main/java/com/ssafy/myissue/user/controller/AuthController.java
+++ backend/my-issue/src/main/java/com/ssafy/myissue/user/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.ssafy.myissue.user.controller;
 
+import com.ssafy.myissue.common.exception.CustomException;
 import com.ssafy.myissue.user.component.TokenResponseWriter;
 import com.ssafy.myissue.user.dto.RegisterDeviceRequest;
 import com.ssafy.myissue.user.dto.TokenPairResponse;
@@ -11,6 +12,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -36,27 +39,13 @@ public class AuthController {
         return ResponseEntity.noContent().build();
     }
 
-//    @PostMapping("/refresh")
-//    public ResponseEntity<Map<String, Object>> rotate(@CookieValue("refreshToken") String refresh) {
-//        var data = store.get(refresh).orElseThrow(() -> new org.springframework.web.server.ResponseStatusException(
-//                org.springframework.http.HttpStatus.UNAUTHORIZED, "invalid refresh"));
-//
-//        // 회전: 기존 토큰 폐기 → 새 토큰 발급/저장
-//        store.delete(refresh);
-//
-//        String jti = UUID.randomUUID().toString();
-//        String access = jwt.createAccess(data.userId(), null, jti);
-//        String newRefresh = jwt.randomRefresh();
-//        store.save(newRefresh, data.userId(), data.uuid(), jti, Duration.ofDays(30));
-//
-//        var cookie = ResponseCookie.from("refreshToken", newRefresh)
-//                .httpOnly(true).secure(true).sameSite("Strict").path("/auth/refresh")
-//                .maxAge(Duration.ofDays(30)).build();
-//
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-//                .header(HttpHeaders.AUTHORIZATION, "Bearer " + access)
-//                .header("Access-Control-Expose-Headers", "Authorization")
-//                .body(Map.of("accessTokenExpiresIn", 900));
-//    }
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> rotateRefresh(@CookieValue("refreshToken") String refresh, HttpServletResponse response) {
+        log.debug("[Refresh 재발급 - CookieValue] refreshToken: {}", refresh);
+
+        TokenPairResponse tokenPairResponse = authService.rotateRefresh(refresh);
+        tokenResponseWriter.write(tokenPairResponse, response);
+
+        return ResponseEntity.noContent().build();
+    }
 }
