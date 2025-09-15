ca3b23b5c9d46a7e1af617a8dd93321c3d13622b
홍시은 <tjwns53@naver.com>
2025-09-15 16:10:39 +0900

    docs: 알림 전체 조회 API swagger 문서 작성
    S13P21D101-83

--- backend/my-issue/src/main/java/com/ssafy/myissue/notification/controller/NotificationController.java
+++ backend/my-issue/src/main/java/com/ssafy/myissue/notification/controller/NotificationController.java
@@ -3,6 +3,12 @@ package com.ssafy.myissue.notification.controller;
 import com.ssafy.myissue.notification.dto.NotificationsResponse;
 import com.ssafy.myissue.notification.dto.SliceResponseDto;
 import com.ssafy.myissue.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +19,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/notification")
+@Tag(name = "Notification", description = "알림(Notification) API")
 public class NotificationController {
 
     private final NotificationService notificationService;
 
     @GetMapping
+    @Operation(
+            summary = "내 알림 전체 조회 API",
+            description = """
+                    - 최신 알림 순으로 size 개수만큼 조회
+                    - lastId가 없으면 가장 최신 알림부터 size 개수만큼 조회
+                    - lastId가 있으면 lastId보다 작은 알림부터 size 개수만큼 조회
+                    """
+    )
     public ResponseEntity<SliceResponseDto<NotificationsResponse>> findAllByUserId(
             @AuthenticationPrincipal Long userId,
             @RequestParam(required = false) Long lastId,
